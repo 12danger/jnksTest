f74dc8390cfe78fafc6dc27fcc9bee7396a71153
12danger <thofi12@gmail.com>
2020-04-10 19:07:22 +0200

    Added Project

---
+++ src/main/java/de/t5f2k/FirstJnknsProject/FirstJnknsProjectApplication.java
@@ -0,0 +1,13 @@
+package de.t5f2k.FirstJnknsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJnknsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJnknsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/t5f2k/FirstJnknsProject/FirstJnknsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.t5f2k.FirstJnknsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJnknsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
